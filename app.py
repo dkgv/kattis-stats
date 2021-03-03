cc836663494a67aa57567439bf70e4af7247e1b2
Gustav <gustav-git@pm.me>
2021-03-03 20:11:39 +0100

    Sort by descending

--- app.py
+++ app.py
@@ -9,25 +9,28 @@ app = Flask(__name__)
 def index():
     all_history = find_all_history()
     all_users = find_all_user_ids()
-    
+
     today = datetime.date.today()
     yesterday = str(today - datetime.timedelta(days=1))
     today = str(today)
-    
+
     history_today = [day for day in all_history if day['Date'] == today]
-    history_yesterday = [day for day in all_history if day['Date'] == yesterday]
-    
+    history_yesterday = [
+        day for day in all_history if day['Date'] == yesterday]
+
     def pts_day(user, history):
         for entry in history:
             if entry['UserId'] == user:
                 return entry['Score']
         return 99999999
-    
+
     pts_gain = {}
     for user in all_users:
-        pts_gain[user] = max(0, pts_day(user, history_today) - pts_day(user, history_yesterday))
-    
-    gains = sorted(pts_gain.items(), key=lambda item: item[1])
+        pts_gain[user] = max(0, pts_day(user, history_today) -
+                             pts_day(user, history_yesterday))
+
+    gains = sorted(pts_gain.items(), key=lambda item: item[1], reverse=True)
+    gains = [{'UserId': gain[0], 'Score': gain[1]} for gain in gains]
     return render_template('index.html', users=all_users, gains=gains[:min(10, len(gains))])
 
 
@@ -44,11 +47,11 @@ def add_user():
         return jsonify({'status': 'error'})
 
     uid = data['uid']
-    
+
     # Does user already exist?
     if any(row['fields']['UserId'] == uid for row in users_table.get_all()):
         return jsonify({'status': 'user already exists'})
-    
+
     users_table.insert({'UserId': uid})
     fetch_for_user(uid)
 
