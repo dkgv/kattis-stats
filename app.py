e413be4015db26d9c8e71224b78b2f076d500a81
Gustav <gustav-git@pm.me>
2021-03-04 15:25:40 +0100

    Scrape and show country and university for each user

--- app.py
+++ app.py
@@ -8,30 +8,43 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     all_history = find_all_history()
-    all_users = find_all_user_ids()
+    all_users = [row['fields'] for row in users_table.get_all()]
 
     today = datetime.date.today()
     yesterday = str(today - datetime.timedelta(days=1))
     today = str(today)
 
-    history_today = [day for day in all_history if day['Date'] == today]
-    history_yesterday = [
-        day for day in all_history if day['Date'] == yesterday]
+    history_today, history_yesterday = [], []
+    for day in all_history:
+        if day['Date'] == today:
+            history_today.append(day)
+        elif day['Date'] == yesterday:
+            history_yesterday.append(day)
 
     def pts_day(user: str, history: list):
         for entry in history:
-            if entry['UserId'] == user:
+            if entry['UserId'] == user['UserId']:
                 return entry['Score']
         return 99999999
 
+    # Compute each user score gain from yesterday -> today
     pts_gain = {}
     for user in all_users:
-        pts_gain[user] = max(0, pts_day(user, history_today) -
-                             pts_day(user, history_yesterday))
+        pts_gain[user['UserId']] = {
+            'Score': max(0, pts_day(user, history_today) -
+                         pts_day(user, history_yesterday)),
+            'User': user
+        }
 
-    gains = sorted(pts_gain.items(), key=lambda item: item[1], reverse=True)
-    gains = [{'UserId': gain[0], 'Score': gain[1]} for gain in gains]
-    return render_template('index.html', users=all_users, gains=gains[:min(10, len(gains))])
+    gains = sorted(pts_gain.items(),
+                   key=lambda item: item[1]['Score'], reverse=True)
+    gains = [{'User': gain[1]['User'], 'Score': gain[1]['Score']}
+             for gain in gains]
+    return render_template(
+        'index.html',
+        users=all_users,
+        gains=gains[:min(10, len(gains))]
+    )
 
 
 @app.route('/view_stats/<uid>')
@@ -59,8 +72,15 @@ def add_user():
     if any(row['fields']['UserId'] == uid for row in users_table.get_all()):
         return jsonify({'status': 'user already exists'})
 
-    users_table.insert({'UserId': uid})
-    fetch_for_user(uid)
+    profile = fetch_for_user(uid)
+    users_table.insert({
+        'UserId': uid,
+        'Name': profile.name,
+        'Country': profile.country,
+        'CountryShort': profile.country_short,
+        'University': profile.university,
+        'UniversityShort': profile.university_short,
+    })
 
     return jsonify({'status': 'success'})
 
--- kattis.py
+++ kattis.py
@@ -11,12 +11,14 @@ def _get_airtable(table: str) -> Airtable:
     api_key = os.environ['AIRTABLE_API_KEY']
     return Airtable('app6pZ1s8sxjhqGH6', api_key=api_key, table_name=table)
 
+
 stats_table = _get_airtable('Stats')
 users_table = _get_airtable('Users')
-Stats = namedtuple('Stats', ['profile_id', 'rank', 'score'])
+Profile = namedtuple('Profile', ['name', 'uid', 'rank',
+                                 'score', 'country', 'country_short', 'university', 'university_short'])
 
 
-def _scrape_stats(uid: str) -> Stats:
+def _scrape_profile(uid: str) -> Profile:
     url = 'https://open.kattis.com/users/{}'.format(uid)
     r = requests.get(url, headers={
                      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0'
@@ -25,7 +27,17 @@ def _scrape_stats(uid: str) -> Stats:
         return None
 
     soup = BeautifulSoup(r.text, 'lxml')
-    name = soup.select_one('.fullname > h1:nth-child(1)')
+    name = soup.title.text.split(' – ')[0]
+
+    country_a = soup.select_one('.text > a:nth-child(1)')
+    country = '' if not country_a else country_a.text
+    country_short = '' if not country_a else country_a['href'].split('/')[-1]
+
+    uni_a = soup.select_one(
+        '.university-logo > span:nth-child(2) > a:nth-child(1)')
+    uni = '' if not uni_a else uni_a.text
+    uni_short = '' if not uni_a else uni_a['href'].split(
+        '/')[-1]
 
     row = soup.find('table').find_all('tr')[1]
     cols = row.find_all('td')
@@ -33,10 +45,10 @@ def _scrape_stats(uid: str) -> Stats:
     rank = cols[0].text.strip()
     score = cols[1].text.strip()
 
-    return Stats(name.text.strip(), rank, score)
+    return Profile(name, uid, rank, score, country, country_short, uni, uni_short)
 
 
-def find_all_history() -> List[Stats]:
+def find_all_history() -> List[Profile]:
     return [row['fields'] for row in stats_table.get_all()]
 
 
@@ -44,14 +56,15 @@ def find_all_user_ids() -> list:
     return [row['fields']['UserId'] for row in users_table.get_all()]
 
 
-def fetch_for_user(uid: str) -> None:
-    stats = _scrape_stats(uid)
+def fetch_for_user(uid: str) -> Profile:
+    profile = _scrape_profile(uid)
     stats_table.insert({
         'UserId': uid,
-        'Rank': int(stats.rank),
-        'Score': float(stats.score),
-        'Date': str(datetime.date.today())
+        'Rank': int(profile.rank),
+        'Score': float(profile.score),
+        'Date': str(datetime.date.today()),
     })
+    return profile
 
 
 if __name__ == '__main__':
